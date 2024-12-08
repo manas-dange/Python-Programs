cbde3cf01249c5d09e626e9e11c2ab267759a3ce
Manas Dange <notify.manas@gmail.com>
2024-12-08 23:23:51 +0530

    Create daysoftheweek.py

---
+++ daysoftheweek.py
@@ -0,0 +1,17 @@
+a = int(input("enter a number form 1 to 7"))
+if a == 1:
+    print("Sunday")
+elif a ==2:
+    print("Monday")
+elif a==3:
+    print("Tuesday")
+elif a==4:
+    print("Wednesday")
+elif a==5:
+    print("Thursday")
+elif a==6:
+    print("Friday")
+elif a==7:
+    print("Saturday")
+else:
+    print("only enter number between 1 to 7")
