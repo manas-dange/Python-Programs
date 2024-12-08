c8eacfdf448d77db9f577b6fbe97fb2874f4ea6b
Manas Dange <notify.manas@gmail.com>
2024-12-08 23:22:27 +0530

    Create ternary_operator_nested_if.py

---
+++ ternary_operator_nested_if.py
@@ -0,0 +1,7 @@
+print("Both a and b are equal" if a==b else "a is greater than b" if a>b else "b is greater than a")
+if a == b:
+    print("Both a and b are equal")
+elif a>b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
